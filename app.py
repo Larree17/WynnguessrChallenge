ed4a321b66e55cb2a684acaad713f4430ab6aa72
BuildTools <unconfigured@null.spigotmc.org>
2024-05-27 20:59:19 -0400

    add button function to index and links to navbar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,3 +9,11 @@ def index():
 @app.route("/play")
 def play():
     return render_template('play.html')
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template('leaderboard.html')
+
+@app.route("/stats")
+def stats():
+    return render_template('stats.html')
